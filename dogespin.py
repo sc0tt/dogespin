225eb7d966cc403e1fef6e085877e4d696c41f52
powhex <powhex@gmail.com>
2013-12-21 04:05:33 +0200

    Update dogespin.py

--- dogespin.py
+++ dogespin.py
@@ -69,23 +69,6 @@ def get_color(col):
 def start_spinner(starting_bet=1, max_bet=64, play=0, qt_loss=100, qt_win=1000, rs=0, col='rand'):
     """
     Start spinner
-
-    'play' = 0: play with real money
-    'play' >= 0: play with play money (value of 'play' is your balance)
-
-    'qt_loss' = 0: play forever
-    'qt_loss' >= 0: play until total loss matches or exceeds value of 'qt_loss'
-
-    'qt_win' = 0: play forever
-    'qt_win' >= 0: play until total win matches or exceeds value of 'qt_win'
-
-    'rs' = 0: use randomized sleep time from 1 to 10 seconds
-    'rs' >= 0: use value of 'rs' as sleep time
-    'rs' = type list: use random list item as sleep time
-
-    'col' = "rand": bet on random color
-    'col' = 0: bet on black
-    'col' = 1: bet on red
     """
     next_color = get_color(col)
     next_bet = starting_bet
